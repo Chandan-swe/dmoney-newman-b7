1e42ed3ab4d1e713775b33f91deb379f69f15db7
Chandan-swe <chandan.swe@gmail.com>
2023-05-24 00:20:40 +0600

    upload

---
+++ report.js
@@ -0,0 +1,18 @@
+const newman = require('newman');
+
+newman.run({
+    collection:require('./collection/dmoney.json'),
+    
+    //environment:require('.collection/env.json'),
+    reporters: 'htmlextra',
+    iterationcount:1,
+    reporter: {
+        htmlextra: {
+            export: './Reports/report.html',
+        }
+    }
+},
+    function (err) {
+        if (err) { throw err; }
+        console.log('collection run complete!');
+    });
